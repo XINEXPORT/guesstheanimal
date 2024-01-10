14e9f148f76dea2b8728411e68faa8e6a3825d16
Christine Hoang <christinehoangt@gmail.com>
2024-01-09 21:34:38 -0600

    seed data changed. audio files now in public folder

--- scripts/seed.js
+++ scripts/seed.js
@@ -12,65 +12,90 @@ await db.sync ({force:true})
 const rooster = await Animal.create({
     animalName: 'Rooster',
     animalImg: '../public/img/rooster.png',
-    animalSound: 'https://actions.google.com/sounds/v1/ambiences/barnyard_with_animals.ogg',
+    animalSound: '../public/audio/roostercrow.wav',
     animalQuestion: 'What sound does a Rooster make?',
     animalDetails: 'Roosters crow all the time! They’ll crow to greet the day, to lead their flocks to forage, to cue a boundary, and to alert about predators.'
 })
-
 const cat = await Animal.create({
     animalName: 'Cat',
     animalImg: '../public/img/cat.png',
-    animalSound: 'https://actions.google.com/sounds/v1/animals/cat_purr.ogg',
+    animalSound: '../public/audio/meow.wav',
     animalQuestion: 'What sound does a Cat make?',
     animalDetails: "Cats are believed to be the only mammals who don’t taste sweetness."
 })
-
 const duck = await Animal.create({
     animalName:'Duck',
     animalImg:'../public/img/duck.png',
-    animalSound:'https://actions.google.com/sounds/v1/animals/geese_outside.ogg',
+    animalSound:'../public/audio/duck.wav',
     animalQuestion:'What sound does a Duck make?',
     animalDetails:'Mallard ducks swim with their tail held above the water. When they are alarmed, they can spring directly out of the water and into the air.'
 })
-
 const pig = await Animal.create({
     animalName:'Pig',
     animalImg:'../public/img/pig.png',
-    animalSound:'https://actions.google.com/sounds/v1/animals/animal_squealing.ogg',
+    animalSound:'../public/audio/pig.mp3',
     animalQuestion:'What sound does a Pig make?',
     animalDetails:'Pigs have the intelligence of a human toddler and are ranked as the fifth most intelligent animal in the world!'
 })
-
 const dog = await Animal.create({
     animalName:'Dog',
     animalImg:'../public/img/dog.png',
-    animalSound:'https://actions.google.com/sounds/v1/animals/dog_barking.ogg',
+    animalSound:'../public/audio/dogbark.wav',
     animalQuestion:'What sound does a Dog make?',
     animalDetails:'Dogs are color-blind. They have trouble seeing shades of red and green but can see blues and yellows.'
 })
-
-const fly = await Animal.create({
-    animalName:'Fly',
-    animalImg:'../public/img/fly.png',
-    animalSound:'https://actions.google.com/sounds/v1/animals/flies_buzzing_and_circling.ogg',
-    animalQuestion:'What sound does a Fly make?',
-    animalDetails:"Flies have sensors on their tarsi, or feet, that help them determine whether that sandwich they just landed on will be a tasty snack."
+const elephant = await Animal.create({
+    animalName:'Elephant',
+    animalImg:'../public/img/elephant.png',
+    animalSound: '../public/audio/elephant.wav',
+    animalQuestion:'What sound does a Elephant make?',
+    animalDetails:'Elephant facts'
 })
-
-const mouse = await Animal.create({
-    animalName:'Mouse',
-    animalImg:'../public/img/mouse.png',
-    animalSound: 'https://actions.google.com/sounds/v1/animals/mouse_squeaking.ogg',
-    animalQuestion:'What sound does a Mouse make?',
-    animalDetails:'Mice are usually nocturnal animals. They have poor eyesight but make up for this with their very good hearing and smell.'
+const lion = await Animal.create({
+    animalName:'Lion',
+    animalImg:'../public/img/lion.png',
+    animalSound: '../public/audio/lion.wav',
+    animalQuestion:'What sound does a Lion make?',
+    animalDetails:'Lion facts'
 })
-
 const owl = await Animal.create({
     animalName:'Owl',
     animalImg:'../public/img/owl.png',
-    animalSound:'https://actions.google.com/sounds/v1/animals/owl_hooting.ogg',
+    animalSound:'../public/audio/owl.mp3',
     animalQuestion:'What sound does a Owl make?',
     animalDetails:'Owls are farsighted, meaning they can’t see things close to their eyes clearly.'
+
+})
+const monkey = await Animal.create({
+    animalName:'Monkey',
+    animalImg:'../public/img/monkey.png',
+    animalSound:'../public/audio/chimpanzee.mp3',
+    animalQuestion:'What sound does a Monkey make?',
+    animalDetails:'Monkey Facts'
+
+})
+const bear = await Animal.create({
+    animalName:'Bear',
+    animalImg:'../public/img/bear.png',
+    animalSound:'../public/audio/bear.mp3',
+    animalQuestion:'What sound does a Bear make?',
+    animalDetails:'Bear Facts'
+
+})
+const snake = await Animal.create({
+    animalName:'Snake',
+    animalImg:'../public/img/snake.png',
+    animalSound:'../public/audio/snake.mp3',
+    animalQuestion:'What sound does a Snake make?',
+    animalDetails:'Snake Facts'
+
+})
+const horse = await Animal.create({
+    animalName:'Horse',
+    animalImg:'../public/img/horse.png',
+    animalSound:'../public/audio/horse.mp3',
+    animalQuestion:'What sound does a Horse make?',
+    animalDetails:'Horse Facts'
 
 })
 
