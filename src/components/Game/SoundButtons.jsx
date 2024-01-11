fa601f8c18960213d27060425c71c8a479b29f73
Christine Hoang <christinehoangt@gmail.com>
2024-01-11 15:17:23 -0600

    magic css buttons on sound buttons

--- src/components/Game/SoundButtons.jsx
+++ src/components/Game/SoundButtons.jsx
@@ -1,21 +1,34 @@
+import React, { useRef } from 'react';
+import { RxSpeakerLoud } from "react-icons/rx";
 import './SoundButtons.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
+const SoundButtonLink = ({ sound, setAnswered, setUserResponse, animalId }) => {
+  const audioRef = useRef(null);
 
-const SoundButtonLink=({sound, setAnswered, setUserResponse, animalId})=>{
-return(
-   <div className='sound-btn'>
-    <audio src={sound} controls></audio>
+  const playSound = () => {
+    audioRef.current.play();
+  };
 
-    <button id="sound-select-btn" onClick={() =>{
-        setUserResponse(animalId)
-        setAnswered(true)
-    }}>
-    <p id = "select-sound">Select</p>
-    </button>
-   </div>
+  return (
+    <div className='sound-btn'>
+      <audio ref={audioRef} src={sound}></audio>
+      <button class = "pushable" onClick={playSound}>
+      <span class="shadow"></span>
+      <span class="edge"></span>
+      <span class="front">
+      <RxSpeakerLoud />
+      </span>
+      </button>
 
-)
+      <button class="sound-select-btn" onClick={() => {
+        setUserResponse(animalId);
+        setAnswered(true);
+      }}>
+        <p id="select-sound">Select</p>
+      </button>
+    </div>
+  );
 }
 
-export default SoundButtonLink
+export default SoundButtonLink;
