9ff6995fa5ca7e3eb9421bf9a99b9e2bbff469f7
Christine Hoang <christinehoangt@gmail.com>
2024-01-17 13:03:20 -0600

    youtube social media added

--- src/components/Index/Home.jsx
+++ src/components/Index/Home.jsx
@@ -4,7 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { SiGlitch } from "react-icons/si";
-
+import { FaYoutube } from "react-icons/fa";
 
 const Home = ({}) => {
     const {animals} =useLoaderData()
@@ -21,6 +21,7 @@ const Home = ({}) => {
             {allAnimals}
             </div>
             <div className = "footer-container">
+            <div className = "chstudios">2024 Christine Hoang Studios. All Rights Reserved. </div>
             <a href="https://www.linkedin.com/in/christine-hoang-profile/" target="_blank" rel="noreferrer">
             <span className = "linkedin-icon"><FaLinkedin /></span>
             </a>
@@ -29,6 +30,9 @@ const Home = ({}) => {
             </a>
             <a href="https://glitch.com/@XINEXPORT" target="_blank" rel="noreferrer">
             <span className = "glitch-icon"><SiGlitch /></span>
+            </a>
+            <a href="https://www.youtube.com/@planetxine" target="_blank" rel="noreferrer">
+            <span className = "youtube-icon"><FaYoutube /></span>
             </a>
         </div>
         </main>
